5baab7a93f364904d448a0b2e6ad68eb0f256a85
Leysly-Mendoza <leyslyymendozaf@gmail.com>
2025-05-20 21:20:04 -0500

    Commit

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,15 @@ const db = mysql.createConnection({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.NAME,
-  port: process.env.DB_PORT || 3306, 
+  port: process.env.DB_PORT || 3306,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("❌ Error al conectar a la base de datos:", err.message);
+    return;
+  }
+  console.log("✅ Conexión a la base de datos exitosa");
 });
 
 module.exports = db;
