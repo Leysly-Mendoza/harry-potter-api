744420def296b71082995a82b5f8b417d5953e57
Leysly-Mendoza <leyslyymendozaf@gmail.com>
2025-05-20 21:41:35 -0500

    LAST COMMIT

--- utils/db.js
+++ utils/db.js
@@ -2,21 +2,19 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.NAME,
+  port: process.env.DB_PORT, // usa 26456
 });
 
-// Conexión con manejo de error
 db.connect((err) => {
   if (err) {
     console.error("❌ Error al conectar a la base de datos:", err.message);
-    process.exit(1); // <- evita que el proceso siga si falla la BD
-  } else {
-    console.log("✅ Conectado a la base de datos.");
+    return;
   }
+  console.log("✅ Conexión a la base de datos exitosa");
 });
 
 module.exports = db;
