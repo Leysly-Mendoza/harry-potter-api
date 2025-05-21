df99ee0ff7256517ab9a056acd22b04ce6debbd7
Leysly-Mendoza <leyslyymendozaf@gmail.com>
2025-05-20 21:55:37 -0500

    Changes commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use((req, res) => {
   res.status(404).json({ error: "Ruta no encontrada" });
 });
 
-const PORT = process.env.DB_PORT;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
